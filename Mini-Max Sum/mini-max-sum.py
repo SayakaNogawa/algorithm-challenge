67c452e906fac429faf048e7c785b5dfc55a099c
Sayaka Nogawa <hello@iamsaya.com>
2020-04-22 23:58:02 +0900

    solved mini max sum py

--- Mini-Max Sum/mini-max-sum.py
+++ Mini-Max Sum/mini-max-sum.py
@@ -8,7 +8,9 @@ import sys
 
 # Complete the miniMaxSum function below.
 def miniMaxSum(arr):
-    
+    min_num = sum(arr) - max(arr)
+    max_num = sum(arr) - min(arr)
+    print(min_num, max_num)
 
 if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
