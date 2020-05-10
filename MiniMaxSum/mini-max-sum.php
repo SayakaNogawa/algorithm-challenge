0f1e5d0a0d67b03a69ad91ed789cd54a4c6ce42c
Sayaka Nogawa <hello@iamsaya.com>
2020-05-10 23:14:57 +0900

    solved mini-max-sum.js

---
+++ MiniMaxSum/mini-max-sum.php
@@ -0,0 +1,17 @@
+<?php
+
+// Complete the miniMaxSum function below.
+function miniMaxSum($arr) {
+
+
+}
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%[^\n]", $arr_temp);
+
+$arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+miniMaxSum($arr);
+
+fclose($stdin);
